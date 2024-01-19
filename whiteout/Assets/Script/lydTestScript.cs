05e2f2d9ab71a386d3059bc2d0e3df06a02cfea2
ragatas1 <emil.sonstaberg@gmail.com>
2024-01-19 16:30:34 +0100

    sjekka lydting, det funka

---
+++ whiteout/Assets/Script/lydTestScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lydTestScript : MonoBehaviour
+{
+    public AudioSource soundEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundEffect.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

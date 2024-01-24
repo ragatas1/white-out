33016ddf44b0d9bc72273c94088f0529cea6022d
ErikBekkemellem <erik.bekkemellem@gmail.com>
2024-01-24 15:07:03 +0100

    eee

--- whiteout/Assets/Script/SpillLydPåStartScript.cs
+++ whiteout/Assets/Script/SpillLydPåStartScript.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SpillLydPåStartScript : MonoBehaviour
 {
     public AudioSource lyd;
+
+
     // Start is called before the first frame update
     void Start()
     {
         lyd.Play();
+        StartCoroutine(byttCene());
     }
 
+    IEnumerator byttCene()
+    {
+        yield return new WaitForSeconds(34);
+        SceneManager.LoadScene("Emilie");
+    }
 }

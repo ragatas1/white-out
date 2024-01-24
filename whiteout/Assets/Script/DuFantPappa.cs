719c5af0cbf4f96e8e195120bcf5683e3675e097
ErikBekkemellem <erik.bekkemellem@gmail.com>
2024-01-24 21:09:02 +0100

    eee

--- whiteout/Assets/Script/DuFantPappa.cs
+++ whiteout/Assets/Script/DuFantPappa.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DuFantPappa : MonoBehaviour
 {
+    public AudioSource lyd;
+
+
     private void OnTriggerEnter(Collider target)
     {
         if (target.tag == "Player")
         {
             Debug.Log("Collided");
+            StartCoroutine(gameOver());
         }
+    }
+
+    IEnumerator gameOver()
+    {
+        yield return new WaitForSeconds(3);
+        lyd.Play();
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("GameOver");
     }
 }
---
+++ whiteout/Assets/Script/TurnOnFog.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnFog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.fog = true;
+    }
+}

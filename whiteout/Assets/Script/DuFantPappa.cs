caa659bb21341f3b0aba4f73746329f4dc1cdfe1
ErikBekkemellem <erik.bekkemellem@gmail.com>
2024-01-29 11:57:53 +0100

    eee

--- whiteout/Assets/Script/DuFantPappa.cs
+++ whiteout/Assets/Script/DuFantPappa.cs
@@ -6,11 +6,18 @@ using UnityEngine.SceneManagement;
 public class DuFantPappa : MonoBehaviour
 {
     public AudioSource lyd;
+    public bool duSÂPappa = false;
 
-
+    private void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            lyd.Play();
+        }
+    }
     private void OnTriggerEnter(Collider target)
     {
-        if (target.tag == "Player")
+        if (target.tag == "Player" && duSÂPappa == true)
         {
             Debug.Log("Collided");
             StartCoroutine(gameOver());
@@ -19,8 +26,6 @@ public class DuFantPappa : MonoBehaviour
 
     IEnumerator gameOver()
     {
-        yield return new WaitForSeconds(2);
-        lyd.Play();
         yield return new WaitForSeconds(2);
         SceneManager.LoadScene("GameOver");
     }

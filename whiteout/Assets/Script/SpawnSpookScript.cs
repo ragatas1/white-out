c58cf91d5788c1ec4a982bff277decf11234a8a6
ragatas1 <emil.sonstaberg@gmail.com>
2024-01-30 15:28:12 +0100

    ja

--- whiteout/Assets/Script/SpawnSpookScript.cs
+++ whiteout/Assets/Script/SpawnSpookScript.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class SpawnSpookScript : MonoBehaviour
 {
+    public GameObject spooks;
+    public Transform rotera;
+    public Transform spiller;
+    public float maksTid;
+    public float minTid;
+    float tid;
+    public bool stfu;
     // Start is called before the first frame update
     void Start()
     {
-        
+        stfu = false;
+        StartCoroutine(skummel());
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!stfu)
+        {
+            rotera.position = spiller.position;
+            rotera.Rotate(0, 1, 0);
+        }
+    }
+    IEnumerator skummel()
+    {
+        tid = Random.Range(minTid, maksTid);
+        yield return new WaitForSeconds(tid);
+        stfu = true;
+        Instantiate(spooks, transform.position, transform.rotation);
+        StartCoroutine(skummel());
+        yield return new WaitForSeconds(5);
+        stfu = false;
     }
 }
